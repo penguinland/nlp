069fd59a18ad509e7ba3b4c1bd6693389d6126ed
Alan <alan.davidson@gmail.com>
2014-04-20 15:52:36 -0400

    Removing cruft

--- Grammar.hs
+++ Grammar.hs
@@ -54,19 +54,6 @@ instance Show NounAttributes where
     show _ = ""  -- Don't bother showing the attributes when showing a Grammar
 
 {-
-testNoun :: (NounAttributes -> Bool) -> Grammar -> Bool
-testNoun test (Noun _ attributes) = test attributes
-testNoun test (NounPhrase _ noun) = testNoun test noun
-testNoun test (ArticledNounPhrase _ nounphrase _) = testNoun test nounphrase
---testNoun test (PrepositionalPhrase _ nounphrase) = testNoun test nounphrase
--- TODO: I don't think an infinitive can be a subject of a verb besides "to be."
--- revisit this later.
-testNoun _ (Infinitive _ _) = True  -- Can be a subject or an object
-testNoun _ grammar = error ("Tried testing non-noun grammar " ++ show grammar ++
-                            "for noun-like properties")
--}
-
-{-
 data PrepositionAttributes = { canFollowVerb :: Bool, canFollowNoun :: Bool }
 
 prepositionFollowsVerb :: Node -> Bool
