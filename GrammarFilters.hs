ad9abd92139b9a6ce80f8b5ba78019dee73d4f82
Alan <alan.davidson@gmail.com>
2014-04-20 16:23:59 -0400

    Removing cruft

--- GrammarFilters.hs
+++ GrammarFilters.hs
@@ -75,9 +75,6 @@ testNoun :: (NounAttributes -> Bool) -> Grammar -> Bool
 testNoun test (Noun _ attributes) = test attributes
 testNoun test (NounPhrase _ noun) = testNoun test noun
 testNoun test (ArticledNounPhrase _ nounphrase _) = testNoun test nounphrase
---testNoun test (PrepositionalPhrase _ nounphrase) = testNoun test nounphrase
--- TODO: I don't think an infinitive can be a subject of a verb besides "to
--- be." revisit this later.
 testNoun _ (Infinitive _ _)  = True  -- Can be a subject or an object
 testNoun _ other = error ("Tried testing non-noun grammar " ++ show other ++
                           " for noun-like properties")
