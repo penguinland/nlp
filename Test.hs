261fdbc35c458a3ca1ff4fa646d2165cbc8269c4
Alan <alan.davidson@gmail.com>
2014-04-19 22:48:40 -0400

    Adding -Wall to Test.hs

--- Test.hs
+++ Test.hs
@@ -1,5 +1,8 @@
+{-# OPTIONS_GHC -Wall #-}
+
 module Test where
 
+import qualified Grammar
 import qualified Lexer
 import qualified Parser
 
@@ -8,5 +11,7 @@ import qualified Parser
 
 -- Text taken from the first grade reading comprehension worksheet at
 -- http://www.superteacherworksheets.com/reading-comp/1st-ball-for-my-dog_TZZMD.pdf
+text1 :: String
 text1 = "my dog found a ball . it was a yellow ball . my dog loves to chew . he chewed the yellow ball . my dog found another ball . it was a red ball . my dog loves to play . he played with the red ball . my dog found another ball . it was a blue ball . my dog loves to run . he ran after the blue ball when I threw it ."
+results1 :: [Grammar.Node]
 results1 = Parser.extractSentences . Lexer.lexNodes $ text1
