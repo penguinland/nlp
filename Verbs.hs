0b04b25e4e2ca70401a442232d4f7ec1371da69e
Alan <alan.davidson@gmail.com>
2014-04-21 20:56:43 -0400

    Actually adding the new Verbs file

---
+++ Verbs.hs
@@ -0,0 +1,55 @@
+{-# OPTIONS_GHC -Wall #-}
+
+module Verbs where
+
+import qualified Data.Set
+
+import Grammar
+import LexerHelpers
+import Rules
+
+normalIntransitiveVerbs :: Data.Set.Set String
+normalIntransitiveVerbs = Data.Set.fromList ["ran", "run"]
+
+normalTransitiveVerbs :: Data.Set.Set String
+normalTransitiveVerbs = Data.Set.fromList ["chew", "chewed", "eat", "found",
+    "help", "like", "love", "play", "played", "threw", "was"]
+
+-- Verbs where the proper conjugation of "it ___" is to add an "es" to the verb
+withEsTransitiveVerbs :: Data.Set.Set String
+withEsTransitiveVerbs = Data.Set.fromList ["brush", "catch", "boss"]
+
+withEsIntransitiveVerbs :: Data.Set.Set String
+withEsIntransitiveVerbs = Data.Set.fromList ["fuss"]
+
+sameConjugation :: [VerbAttributes]
+sameConjugation = [ VerbAttributes First False
+                  , VerbAttributes Second False
+                  , VerbAttributes Other False
+                  , VerbAttributes First True
+                  , VerbAttributes Second True
+                  , VerbAttributes Third True]
+
+conjugateVerb ::
+    Data.Set.Set String -> String -> String -> [Rule] -> [Node] -> [Node]
+conjugateVerb list ending word rules next =
+    if Data.Set.member word list
+    then map (\a -> Node (Verb word a) rules next) sameConjugation
+    else case getRootFrom ending word of
+        Just root | Data.Set.member root list ->
+            [Node (Verb word (VerbAttributes Third False)) rules next]
+        _ -> []
+
+makeIntVerb :: String -> [Node] -> [Node]
+makeIntVerb word next =
+    concatMap (\(s, e) -> conjugateVerb s e word intVerbRules next)
+        [(normalIntransitiveVerbs, "s"), (withEsIntransitiveVerbs, "es")]
+
+-- TODO: Can you think of a verb that *requires* a direct object?
+makeTransVerb :: String -> [Node] -> [Node]
+makeTransVerb word next =
+  let
+    verbRules = intVerbRules ++ transVerbRules
+  in
+    concatMap (\(s, e) -> conjugateVerb s e word verbRules next)
+        [(normalTransitiveVerbs, "s"), (withEsTransitiveVerbs, "es")]
