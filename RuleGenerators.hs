1d8707092c18863d2fbc452c005feb7b5eceaec6
Alan <alan.davidson@gmail.com>
2014-04-24 18:10:04 -0400

    Refactoring the Rule generators

--- RuleGenerators.hs
+++ RuleGenerators.hs
@@ -19,35 +19,38 @@ makeRule1 isCorrectGrammar toNewGrammar nextRules =
   in
     newRule
 
--- The Rules for the new Node are the rules for the last Node merged.
-makeRule2AdoptingRules :: (Grammar -> Bool) -> (Grammar -> Bool) ->
-             (Grammar -> Grammar -> Bool) ->
-             (Grammar -> Grammar -> Grammar) -> Rule
-makeRule2AdoptingRules isCorrectGrammar1 isCorrectGrammar2 grammarsAreCompatible
-        toNewGrammar =
+makeRule2ArbitraryRules :: (Node -> Node -> [Rule]) ->
+                           (Grammar -> Bool) -> (Grammar -> Bool) ->
+                           (Grammar -> Grammar -> Bool) ->
+                           (Grammar -> Grammar -> Grammar) -> Rule
+makeRule2ArbitraryRules makeNewRules isCorrectGrammar1 isCorrectGrammar2
+        grammarsAreCompatible toNewGrammar =
   let
     toNewNode first second = Node (toNewGrammar first second)
-    newRule (Node first _ seconds) =
-        [toNewNode first second rules next | isCorrectGrammar1 first,
-         (Node second rules next) <- seconds, isCorrectGrammar2 second,
+    newRule firstNode@(Node first _ seconds) =
+        [toNewNode first second (makeNewRules firstNode secondNode) next |
+         isCorrectGrammar1 first,
+         secondNode@(Node second rules next) <- seconds,
+         isCorrectGrammar2 second,
          grammarsAreCompatible first second]
   in
     newRule
 
+-- The Rules for the new Node are the rules for the last Node merged.
+makeRule2AdoptingRules :: (Grammar -> Bool) -> (Grammar -> Bool) ->
+             (Grammar -> Grammar -> Bool) ->
+             (Grammar -> Grammar -> Grammar) -> Rule
+makeRule2AdoptingRules =
+    makeRule2ArbitraryRules (\_ (Node _ rules _) -> rules)
+
+-- The rules for the new node are given as a static list
 makeRule2 :: (Grammar -> Bool) -> (Grammar -> Bool) ->
              (Grammar -> Grammar -> Bool) ->
              (Grammar -> Grammar -> Grammar) -> [Rule] -> Rule
 makeRule2 isCorrectGrammar1 isCorrectGrammar2 grammarsAreCompatible
         toNewGrammar nextRules =
-  let
-    toNewNode first second next =
-        Node (toNewGrammar first second) nextRules next
-    newRule (Node first _ seconds) =
-        [toNewNode first second next | isCorrectGrammar1 first,
-         (Node second _ next) <- seconds, isCorrectGrammar2 second,
-         grammarsAreCompatible first second]
-  in
-    newRule
+    makeRule2ArbitraryRules (const . const $ nextRules) isCorrectGrammar1
+        isCorrectGrammar2 grammarsAreCompatible toNewGrammar
 
 makeRule3 :: (Grammar -> Bool) -> (Grammar -> Bool) -> (Grammar -> Bool) ->
              (Grammar -> Grammar -> Grammar -> Bool) ->
